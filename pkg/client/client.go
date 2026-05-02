50a9f2f4e0dbadba4d132c01664f0f23c33833ff
Valentin <medvedev.viu@yandex.ru>
2026-05-02 15:12:10 +1000

    add echo for forbidden and allowed resources

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -9,14 +9,14 @@ import (
 )
 
 type RequestResults []struct {
-	URL          string `json:"url"`
-	Ping         int    `json:"ping"`
+	URL  string `json:"url"`
+	Ping int    `json:"ping"`
 	Code string `json:"code"`
 }
 
 type RequestResult struct {
-	URL          string `json:"url"`
-	Ping         int    `json:"ping"`
+	URL  string `json:"url"`
+	Ping int    `json:"ping"`
 	Code string `json:"code"`
 }
 
@@ -28,6 +28,22 @@ type Cloud struct {
 	URL string `yaml:"url"`
 }
 
+type ForbiddenResources []struct {
+	URL string `yaml:"url"`
+}
+
+type ForbiddenResource struct {
+	URL string `yaml:"url"`
+}
+
+type AllowedResources []struct {
+	URL string `yaml:"url"`
+}
+
+type AllowedResource struct {
+	URL string `yaml:"url"`
+}
+
 type job struct {
 	index int
 	url   string
@@ -36,19 +52,6 @@ type job struct {
 type result struct {
 	index int
 	resp  RequestResult
-}
-
-func GetCloudsInfo() (clouds Clouds) {
-	yamlFile, error := os.ReadFile("clouds.yaml")
-	if error != nil {
-		panic(error)
-	}
-	error = yaml.Unmarshal(yamlFile, &clouds)
-	if error != nil {
-		panic(error)
-	}
-
-	return
 }
 
 func Get(url string) (requestResult RequestResult) {
@@ -79,6 +82,19 @@ func Get(url string) (requestResult RequestResult) {
 	return
 }
 
+func GetCloudsInfo() (clouds Clouds) {
+	yamlFile, error := os.ReadFile("clouds.yaml")
+	if error != nil {
+		panic(error)
+	}
+	error = yaml.Unmarshal(yamlFile, &clouds)
+	if error != nil {
+		panic(error)
+	}
+
+	return
+}
+
 func PingClouds() RequestResults {
 	cloudsInfo := GetCloudsInfo()
 	responses := make(RequestResults, len(cloudsInfo))
@@ -98,6 +114,106 @@ func PingClouds() RequestResults {
 	}
 
 	for i, cloud := range cloudsInfo {
+		jobs <- job{i, cloud.URL}
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		responses[res.index] = res.resp
+	}
+
+	return responses
+}
+
+func GetForbiddenInfo() (forbiddenResources ForbiddenResources) {
+	yamlFile, error := os.ReadFile("forbidden.yaml")
+	if error != nil {
+		panic(error)
+	}
+	error = yaml.Unmarshal(yamlFile, &forbiddenResources)
+	if error != nil {
+		panic(error)
+	}
+
+	return
+}
+
+func PingForbidden() RequestResults {
+	forbiddenInfo := GetForbiddenInfo()
+	forbiddenLength := len(forbiddenInfo)
+
+	responses := make(RequestResults, forbiddenLength)
+
+	jobs := make(chan job, forbiddenLength)
+	results := make(chan result, forbiddenLength)
+
+	var wg sync.WaitGroup
+	for range 5 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				results <- result{job.index, Get(job.url)}
+			}
+		}()
+	}
+
+	for i, cloud := range forbiddenInfo {
+		jobs <- job{i, cloud.URL}
+	}
+	close(jobs)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	for res := range results {
+		responses[res.index] = res.resp
+	}
+
+	return responses
+}
+
+func GetAllowedInfo() (allowedResources AllowedResources) {
+	yamlFile, error := os.ReadFile("allowed.yaml")
+	if error != nil {
+		panic(error)
+	}
+	error = yaml.Unmarshal(yamlFile, &allowedResources)
+	if error != nil {
+		panic(error)
+	}
+
+	return
+}
+
+func PingAllowed() RequestResults {
+	allowedInfo := GetAllowedInfo()
+	allowedLength := len(allowedInfo)
+
+	responses := make(RequestResults, allowedLength)
+
+	jobs := make(chan job, allowedLength)
+	results := make(chan result, allowedLength)
+
+	var wg sync.WaitGroup
+	for range 5 {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for job := range jobs {
+				results <- result{job.index, Get(job.url)}
+			}
+		}()
+	}
+
+	for i, cloud := range allowedInfo {
 		jobs <- job{i, cloud.URL}
 	}
 	close(jobs)
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -8,11 +8,7 @@ import (
 	"github.com/medvedev-v/clouds-echo/pkg/client"
 )
 
-type RequestWeather struct {
-	Location string `json:"location"`
-}
-
-func handlePingAllRequest(writer http.ResponseWriter, request *http.Request) {
+func handleEchoCloudsRequest(writer http.ResponseWriter, request *http.Request) {
 	if request.Method != http.MethodGet {
 		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -28,8 +24,42 @@ func handlePingAllRequest(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
+func handleEchoForbiddenRequest(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	response := client.PingForbidden()
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	if error := json.NewEncoder(writer).Encode(response); error != nil {
+		http.Error(writer, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleEchoAllowedRequest(writer http.ResponseWriter, request *http.Request) {
+	if request.Method != http.MethodGet {
+		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	response := client.PingAllowed()
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	if error := json.NewEncoder(writer).Encode(response); error != nil {
+		http.Error(writer, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
 func StartAndServe() {
-	http.HandleFunc("/echo/all", handlePingAllRequest)
+	http.HandleFunc("/echo/clouds", handleEchoCloudsRequest)
+	http.HandleFunc("/echo/forbidden", handleEchoForbiddenRequest)
+	http.HandleFunc("/echo/allowed", handleEchoAllowedRequest)
 
 	log.Println("Server is running on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
